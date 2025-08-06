fdb81c493dec64d144c09e94efc7314d14b5a912
Kaan 87 <seyhunkaan87@gmail.com>
2025-08-06 23:39:41 +0200

    Ergänzt: '/' leitet jetzt zu /tasks (im selben Controller)

--- src/main/java/de/schule/taskliste/TaskController.java
+++ src/main/java/de/schule/taskliste/TaskController.java
@@ -3,6 +3,7 @@ package de.schule.taskliste;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -16,5 +17,15 @@ public class TaskController {
                 new Task(1L, "Test", "Beispiel-Task", false),
                 new Task(2L, "Noch ein Task", "Beschreibung", true)
         );
+    }
+
+}
+
+
+@RestController
+class RootController {
+    @GetMapping("/")
+    public RedirectView redirectToTasks() {
+        return new RedirectView("/tasks");
     }
 }

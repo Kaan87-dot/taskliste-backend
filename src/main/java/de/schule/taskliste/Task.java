c0521886e1babd28784a5b0a53e6beefc469f526
Kaan 87 <seyhunkaan87@gmail.com>
2025-08-08 00:06:28 +0200

    Fix: @Entity ergänzt und Dateikonflikte beseitigt

--- src/main/java/de/schule/taskliste/Task.java
+++ src/main/java/de/schule/taskliste/Task.java
@@ -1,3 +1,11 @@
+package de.schule.taskliste;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

8f61bb93ff4b75293ff8c6a7cefcec1603348b6f
Kaan 87 <seyhunkaan87@gmail.com>
2025-09-12 17:27:27 +0200

    Backend: Task-Entity erweitert um dueDate

--- src/main/java/de/schule/taskliste/Task.java
+++ src/main/java/de/schule/taskliste/Task.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import java.time.LocalDate; 
 
 @Entity
 public class Task {
@@ -17,13 +18,12 @@ public class Task {
 
     public Task() {}
 
-    public Task(String title, String description, boolean done) {
+    public Task(String title, String description, boolean done, LocalDate dueDate) {
         this.title = title;
         this.description = description;
         this.done = done;
+        this.dueDate = dueDate;
     }
-
-
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -36,4 +36,7 @@ public class Task {
 
     public boolean isDone() { return done; }
     public void setDone(boolean done) { this.done = done; }
+
+    public LocalDate getDueDate() { return dueDate; }
+    public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate; }
 }

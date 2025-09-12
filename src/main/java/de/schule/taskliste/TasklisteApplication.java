e874ece91a118fae3c413571ac4ac63a06276e90
Kaan 87 <seyhunkaan87@gmail.com>
2025-09-12 02:51:52 +0200

    Taskanzeige verbessert, Backend-Ausgabe entfernt

--- src/main/java/de/schule/taskliste/TasklisteApplication.java
+++ src/main/java/de/schule/taskliste/TasklisteApplication.java
@@ -2,21 +2,10 @@ package de.schule.taskliste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class TasklisteApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TasklisteApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner initData(TaskRepository repository) {
-		return args -> {
-			repository.save(new Task("Testaufgabe", "Beschreibung", false));
-			repository.save(new Task("Noch eine Aufgabe", "Zweite Beschreibung", true));
-		};
 	}
 }
